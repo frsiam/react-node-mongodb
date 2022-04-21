93655bdaddb6edb4904d87ad311af641fe06b75e
frsiam <siam.frs@gmail.com>
2022-04-21 16:50:19 +0600

    data added from client site

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
 const AddUser = () => {
+
+    const handleAddUser = (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+
+        const user = {name, email};
+
+        // send data to the server
+        fetch('http://localhost:5000/user',{
+            method: 'post',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('success',data)
+            alert('Successfully added a user')
+            event.target.reset();
+        })
+    }
     return (
-        <div>
+        <div className='mt-5'>
             <h1>Please Add User</h1>
-            <form className='border border-dark p-3 w-50 mx-auto my-5'>
+            <form onSubmit={handleAddUser} className='border border-dark p-3 w-50 mx-auto my-5'>
                 <div className="mb-3">
                     <input name='name' type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Name'/>
                 </div>
