548c2519446c0c7ab54f05b7e38b789b204fbc42
frsiam <siam.frs@gmail.com>
2022-04-21 15:27:24 +0600

    install bootstrap and add a simple bootstrap form

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -4,6 +4,15 @@ const AddUser = () => {
     return (
         <div>
             <h1>Please Add User</h1>
+            <form className='border border-dark p-3 w-50 mx-auto my-5'>
+                <div className="mb-3">
+                    <input name='name' type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Name'/>
+                </div>
+                <div className="mb-3">
+                    <input name='email' type="email" className="form-control" id="exampleInputPassword1" placeholder='Email'/>
+                </div>
+                <button type="submit" className="btn btn-primary">Add User</button>
+            </form>
         </div>
     );
 };
